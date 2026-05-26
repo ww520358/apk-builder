bf18ad651ad86e2a5e0f6c3699758520c76e8a5c
ww520358 <640515393@qq.com>
2026-05-27 00:28:50 +0800

    Extract: app/build/gen/com/tencent/tim/BuildConfig.java

---
+++ app/build/gen/com/tencent/tim/BuildConfig.java
@@ -0,0 +1,10 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.tencent.tim;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+    public final static String APPLICATION_ID = "com.tencent.tim";
+    public final static String BUILD_TYPE = "debug";
+    public final static long VERSION_CODE = 1;
+    public final static String VERSION_NAME = "1.0";
+}
